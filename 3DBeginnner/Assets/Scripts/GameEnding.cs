ec4d1ea536b97b42940fec918082b9eaab5a7e0d
cpan <axxnxx@163.com>
2019-06-13 11:32:02 +0800

    Ending the Game

---
+++ 3DBeginnner/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+
+
+    private bool _isPlayerAtExit = false;
+    private float _timer;
+    
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == this.player)
+        {
+            this._isPlayerAtExit = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (this._isPlayerAtExit)
+        { 
+            this.EndLevel();
+        }
+    }
+
+    private void EndLevel()
+    {
+        this._timer += Time.deltaTime;
+        exitBackgroundImageCanvasGroup.alpha = _timer / fadeDuration;
+        if (_timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
+    }
+}
